0bc7e1078a65421c6171ffe94efab196a77e1e5b
Hallory <khalpeep@gmail.com>
2024-12-19 12:51:31 +0100

    add redux provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import ReduxProvider from "./store/ReduxProvider";
 
 export const metadata: Metadata = {
   title: "Marketplace",
   description: "Its a marketplace",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-       
-      >
-        {children}
+      <body>
+        <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
   );
